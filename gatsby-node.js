f5b1a244a0b91b91c5137ffe92d551566bd43b47
Harry Phan <cuoiboy@gmail.com>
2019-12-04 17:38:15 +1100

    Add square profile picture to persone page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,96 +4,104 @@ const path = require('path');
 const DirectoryNamedWebpackPlugin = require('directory-named-webpack-plugin');
 
 exports.onCreateWebpackConfig = ({
-  stage,
-  getConfig,
-  rules,
-  loaders,
-  actions,
+	stage,
+	getConfig,
+	rules,
+	loaders,
+	actions,
 }) => {
-  const config = getConfig()
-  if (stage.startsWith('develop') && config.resolve) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      'react-dom': '@hot-loader/react-dom'
-    }
-  }
-  
-  actions.setWebpackConfig({
-    resolve: {
-      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-      plugins: [new DirectoryNamedWebpackPlugin({
-        exclude: /node_modules/
-      })],
-    },
-  });
+	const config = getConfig();
+	if (stage.startsWith('develop') && config.resolve) {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'react-dom': '@hot-loader/react-dom',
+		}
+	}
+
+	actions.setWebpackConfig({
+		resolve: {
+			modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+			plugins: [
+				new DirectoryNamedWebpackPlugin({
+					exclude: /node_modules/,
+				}),
+			],
+		},
+	});
 };
 
 exports.sourceNodes = async ({ actions }) => {
-  const { createNode } = actions;
+	const { createNode } = actions;
 
-  const userSkillsResult = await axios({
-    method: 'get',
-    url: process.env.SOPHIE_API_URL,
-    data: {},
-    headers: {
-      Authorization: process.env.SOPHIE_AUTHORIZATION,
-      Tenant: process.env.SOPHIE_TENANT
-    }
-  });
-  
-  userSkillsResult.data.map((user, i) => {
-    const userNode = {
-      id: user.userId,
-      parent: `__SOURCE__`,
-      internal: {
-        type: `UserSkillsCollection`,
-      },
-      children: [],
+	const userSkillsResult = await axios({
+		method: 'get',
+		url: process.env.SOPHIE_API_URL,
+		data: {},
+		headers: {
+			Authorization: process.env.SOPHIE_AUTHORIZATION,
+			Tenant: process.env.SOPHIE_TENANT,
+		},
+	});
 
-      slug: `${user.firstName}-${user.lastName}`,
-      fullName: `${user.firstName} ${user.lastName}`,
-      emailAddress: user.emailAddress,
-      location: user.defaultSite.name,
-      intermediateSkills: user.skills.filter(s => s.experienceLevel === 'Intermediate').map(s => s.technology),
-      advancedSkills: user.skills.filter(s => s.experienceLevel === 'Advanced').map(s => s.technology),
-    };
+	userSkillsResult.data.map((user, i) => {
+		const userNode = {
+			id: user.userId,
+			parent: `__SOURCE__`,
+			internal: {
+				type: `UserSkillsCollection`,
+			},
+			children: [],
 
-    // Get content digest of node. (Required field)
-    const contentDigest = crypto
-      .createHash(`md5`)
-      .update(JSON.stringify(userNode))
-      .digest(`hex`);
-    // add it to userNode
-    userNode.internal.contentDigest = contentDigest;
+			slug: `${user.firstName}-${user.lastName}`,
+			fullName: `${user.firstName} ${user.lastName}`,
+			emailAddress: user.emailAddress,
+			location: user.defaultSite.name,
+			intermediateSkills: user.skills
+				.filter(s => s.experienceLevel === 'Intermediate')
+				.map(s => s.technology),
+			advancedSkills: user.skills
+				.filter(s => s.experienceLevel === 'Advanced')
+				.map(s => s.technology),
+		};
 
-    // Create node with the gatsby createNode() API
-    createNode(userNode);
-  });
+		// Get content digest of node. (Required field)
+		const contentDigest = crypto
+			.createHash(`md5`)
+			.update(JSON.stringify(userNode))
+			.digest(`hex`);
+		// add it to userNode
+		userNode.internal.contentDigest = contentDigest;
 
-  return;
-}
+		// Create node with the gatsby createNode() API
+		createNode(userNode);
+	});
+
+	return;
+};
 
 exports.createPages = async function({ actions, graphql }) {
-  const { data } = await graphql(`
-    query {
-      people: allFile(filter: {sourceInstanceName: {eq: "people"}}) {
-        edges {
-          node {
-            name
-          }
-        }
-      }
-    }
-  `);
+	const { data } = await graphql(`
+		query {
+			people: allFile(filter: { sourceInstanceName: { eq: "people" } }) {
+				edges {
+					node {
+						name
+					}
+				}
+			}
+		}
+	`);
 
-  data.people.edges.forEach(edge => {
+	data.people.edges.forEach(edge => {
     const slug = edge.node.name;
-    actions.createPage({
-      path: slug,
-      component: require.resolve(`./src/templates/person.js`),
-      context: { 
-        slug: slug 
-      },
-    });
-  });
+    const squareImage = slug + '-Profile-Square';
+		actions.createPage({
+			path: slug,
+			component: require.resolve(`./src/templates/person.js`),
+			context: {
+				slug: slug,
+				squareImage: squareImage,
+			},
+		});
+	});
 };
--- src/templates/person.js
+++ src/templates/person.js
@@ -4,49 +4,58 @@ import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
 const Person = ({ data }) => {
-  const person = data.people;
-  const childMarkdownRemark = person.childMarkdownRemark || {};
+	const person = data.people;
+	const childMarkdownRemark = person.childMarkdownRemark || {};
 	const frontmatter = childMarkdownRemark.frontmatter || {};
 	const profileHtml = childMarkdownRemark.html || {};
-  const skills = data.skills || {};
-  const intermediateSkills = skills.advancedSkills || [];
-  const advancedSkills = skills.advancedSkills || [];
+	const skills = data.skills || {};
+	const intermediateSkills = skills.advancedSkills || [];
+	const advancedSkills = skills.advancedSkills || [];
+	const profileImage = data.profileImage.nodes[0];
 
 	return (
 		<Layout>
-			<div>
-				<h1>{frontmatter.name}</h1>
-				<dl>
-					<dt>Position</dt>
-					<dd>{frontmatter.role}</dd>
-				</dl>
-				<dl>
-					<dt>Qualification</dt>
-					<dd>{frontmatter.qualifications}</dd>
-				</dl>
-				<dl>
-					<dt>Location</dt>
-					<dd>{frontmatter.location}</dd>
-				</dl>
-				<dl>
-          <dt>Skills</dt>
-          {
-            advancedSkills.map((skill, i) => (
-              <dd key={`a${i}`}>{skill}</dd>
-            ))
-          }
-          {
-            intermediateSkills.map((skill, i) => (
-              <dd key={`i${i}`}>{skill}</dd>
-            ))
-          }
-				</dl>
+			<div className="flex mb-4">
+				<div className="w-1/4">
+					<div
+						className="relative bg-cover shadow-lg mx-auto"
+						style={{
+							backgroundImage: `url(${profileImage.childImageSharp.original.src})`,
+							height: `${profileImage.childImageSharp.original.height}px`,
+							width: `${profileImage.childImageSharp.original.width}px`,
+						}}
+					></div>
+				</div>
+				<div className="w-3/4">
+					<h1>{frontmatter.name}</h1>
+					<dl>
+						<dt>Position:</dt>
+						<dd>{frontmatter.role}</dd>
+					</dl>
+					<dl>
+						<dt>Qualification:</dt>
+						<dd>{frontmatter.qualifications}</dd>
+					</dl>
+					<dl>
+						<dt>Location:</dt>
+						<dd>{frontmatter.location}</dd>
+					</dl>
+					<dl>
+						<dt>Skills:</dt>
+						{advancedSkills.map((skill, i) => (
+							<dd key={`a${i}`}>{skill}</dd>
+						))}
+						{intermediateSkills.map((skill, i) => (
+							<dd key={`i${i}`}>{skill}</dd>
+						))}
+					</dl>
 
-				<div
-					dangerouslySetInnerHTML={{
-						__html: profileHtml,
-					}}
-				/>
+					<div
+						dangerouslySetInnerHTML={{
+							__html: profileHtml,
+						}}
+					/>
+				</div>
 			</div>
 		</Layout>
 	);
@@ -59,7 +68,7 @@ Person.propTypes = {
 export default Person;
 
 export const query = graphql`
-	query($slug: String!) {
+	query($slug: String!, $squareImage: String!) {
 		people: file(sourceInstanceName: { eq: "people" }, name: { eq: $slug }) {
 			name
 			childMarkdownRemark {
@@ -85,6 +94,23 @@ export const query = graphql`
 		skills: userSkillsCollection(slug: { eq: $slug }) {
 			intermediateSkills
 			advancedSkills
+		}
+		profileImage: allFile(
+			filter: {
+				sourceInstanceName: { eq: "people" }
+				name: { glob: $squareImage }
+			}
+		) {
+			nodes {
+				name
+				childImageSharp {
+					original {
+						height
+						src
+						width
+					}
+				}
+			}
 		}
 	}
 `;
