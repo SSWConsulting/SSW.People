c2a6bff519ae8e311a0583d58e18ab90041d4057
Harry Phan <cuoiboy@gmail.com>
2019-12-20 16:42:19 +1100

    Fix logo

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
 import posed from 'react-pose';
-import sswLogo from '../../images/SSWLogo.svg';
+import SSWLogo from '-!svg-react-loader!../../images/SSWLogo.svg';
 
 // Example of a component-specific page transition
 const AnimatedContainer = posed.div({
@@ -26,7 +26,7 @@ const Header = ({ title }) => (
 			<div className="flex flex-wrap md:flex-no-wrap items-center justify-between max-w-6xl mx-auto p-4 md:p-8">
 				<div className="flex items-center">
 					<Link to="/">
-						<img src={sswLogo} alt="SSW logo"></img>
+						<SSWLogo ariaLabel="logo" />
 					</Link>
 					<h1 className="title">Our People</h1>
 				</div>
