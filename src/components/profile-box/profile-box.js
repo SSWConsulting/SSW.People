9b1f4f328e24d33d79b479b963de4fd052aa2a94
Jean Thirion <jeanthirion@ssw.com.au>
2020-01-09 17:10:06 +1100

    Implemented we're hiring link

--- src/components/profile-box/profile-box.js
+++ src/components/profile-box/profile-box.js
@@ -3,40 +3,49 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 
 const ProfileBox = ({ profile, sanitisedName, profileImages }) => {
-	const [hover, setHover] = useState(false);
+  const [hover, setHover] = useState(false);
 
-	return (
+
+  const content = profileImages.profileImage !== undefined && (
+    <div
+      className="relative bg-cover shadow-lg profile-image"
+      style={{
+        backgroundImage: `url(${
+          hover
+            ? profileImages.profileImage
+            : profileImages.sketchProfileImage
+        })`,
+        height: '242px',
+        width: '172px',
+      }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <div
+        className={
+          hover
+            ? 'absolute inset-x-0 bottom-0 px-1 pb-4 pt-2 h-15 text-center hovered'
+            : 'absolute inset-x-0 bottom-0 px-1 pb-4 pt-2 h-15 text-center'
+        }
+      >
+        <div className="font-bold text-sm">{profile.nickname}</div>
+        <div className="text-xs leading-none">{profile.role}</div>
+      </div>
+    </div>
+  );
+
+  return (profile.role === "enthusiastic People" ?
+    <a
+      href='https://www.ssw.com.au/ssw/Employment/default.aspx'
+      className='w-full flex-profile-box mr-4 mb-4 unstyled' >
+      {content}
+    </a>
+    :
 		<Link
 			to={`/${sanitisedName}`}
 			className="w-full flex-profile-box mr-4 mb-4 unstyled"
 		>
-			{profileImages.profileImage !== undefined && (
-				<div
-					className="relative bg-cover shadow-lg profile-image"
-					style={{
-						backgroundImage: `url(${
-							hover
-								? profileImages.profileImage
-								: profileImages.sketchProfileImage
-						})`,
-						height: '242px',
-						width: '172px',
-					}}
-					onMouseEnter={() => setHover(true)}
-					onMouseLeave={() => setHover(false)}
-				>
-					<div
-						className={
-							hover
-								? 'absolute inset-x-0 bottom-0 px-1 pb-4 pt-2 h-15 text-center hovered'
-								: 'absolute inset-x-0 bottom-0 px-1 pb-4 pt-2 h-15 text-center'
-						}
-					>
-						<div className="font-bold text-sm">{profile.nickname}</div>
-						<div className="text-xs leading-none">{profile.role}</div>
-					</div>
-				</div>
-			)}
+      {content}
 		</Link>
 	);
 };
