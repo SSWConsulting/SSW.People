c62abead845ff5e17f4095391fd51e5ebe85d4b9
TiSeb <boissiere.seb@gmail.com>
2020-01-23 14:45:28 +1100

    Add new component for contact button

---
+++ src/components/contact/contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Contact = ({ profileName }) => {
+  return (
+    <div className="contact">
+      <h3 className="text-ssw-red">
+        Interested in what {profileName} can do for you?
+      </h3>
+      <p>
+        Jump on a call with one of our Account Managers to discuss how we can
+        help you.
+      </p>
+      <button className="btn">Book a Free Initial Meeting</button>
+      <p>or call on +61 2 9953 3000</p>
+    </div>
+  );
+};
+
+Contact.propTypes = {};
+
+export default Contact;
