21c7bf8267577656fcbabb1f515bf8037d6d3197
TiSeb <boissiere.seb@gmail.com>
2020-01-30 11:09:28 +1100

    Fix lint warnings on contact form

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Contact = ({onClick, profileName,}) => {
-
+const Contact = ({ onClick, profileName }) => {
   return (
     <div className="contact">
       <h3 className="text-ssw-red">
         Interested in what {profileName} can do for you?
       </h3>
+      <p>Contact an Account Manager to discuss your project</p>
+      <button className="btn" onClick={() => onClick()}>
+        Book a Free Initial Meeting
+      </button>
       <p>
-      Contact an Account Manager to discuss your project
+        or call on <span className="font-semibold">+61 2 9953 3000</span>
       </p>
-      <button className="btn" onClick={() => onClick()}>Book a Free Initial Meeting</button>
-      <p>or call on <span className="font-semibold">+61 2 9953 3000</span></p>
     </div>
   );
 };
