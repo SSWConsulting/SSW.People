0fbe31d2d15489a4a936f6d64127f9af2e54484a
patriciabarrosssw <patriciabarros@ssw.com.au>
2020-01-30 15:08:25 +1000

    Added Blog Link

--- src/templates/person.js
+++ src/templates/person.js
@@ -64,7 +64,7 @@ const Person = ({ data }) => {
                 <div className="w-full pr-2 lg:hidden quoteblock">
                   <div className="person-quote">{frontmatter.quote}</div>
                   <div className="person-quote-name">
-                    {frontmatter.quote_author
+                    {frontmatter.quote_author != ''
                       ? frontmatter.quote_author
                       : frontmatter.nickname}
                   </div>
@@ -76,7 +76,7 @@ const Person = ({ data }) => {
             <div className="hidden w-1/2 pr-2 lg:pr-0 lg:w-full lg:block quoteblock">
               <div className="person-quote">{frontmatter.quote}</div>
               <div className="person-quote-name">
-                {frontmatter.quote_author
+                {frontmatter.quote_author != ''
                   ? frontmatter.quote_author
                   : frontmatter.nickname}
               </div>
@@ -88,11 +88,16 @@ const Person = ({ data }) => {
                     <a href={'mailto:' + crmData.emailAddress}>Email</a>
                   </li>
                 )}
+                {frontmatter.blog != '' && (
+                  <li id="blog" className="social">
+                    <a target="_blank" rel="noopener noreferrer" href={frontmatter.blog}>
+                      Blog
+                    </a>
+                  </li>
+                )}
                 {frontmatter.facebook != '' && (
                   <li id="facebook" className="social">
-                    <a
-                      href={'https://www.facebook.com/' + frontmatter.facebook}
-                    >
+                    <a target="_blank" rel="noopener noreferrer" href={'https://www.facebook.com/' + frontmatter.facebook}>
                       Facebook
                     </a>
                   </li>
@@ -104,18 +109,14 @@ const Person = ({ data }) => {
                 )}
                 {frontmatter.linkedin != '' && (
                   <li id="linkedin" className="social">
-                    <a
-                      href={
-                        'https://www.linkedin.com/in/' + frontmatter.linkedin
-                      }
-                    >
+                    <a target="_blank" rel="noopener noreferrer" href={'https://www.linkedin.com/in/' + frontmatter.linkedin}>
                       LinkedIn
                     </a>
                   </li>
                 )}
                 {frontmatter.twitter != '' && (
                   <li id="twitter" className="social">
-                    <a href={'https://www.twitter.com/' + frontmatter.twitter}>
+                    <a target="_blank" rel="noopener noreferrer" href={'https://www.twitter.com/' + frontmatter.twitter}>
                       Twitter
                     </a>
                   </li>
