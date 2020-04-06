57ad21c4bab29569ae6a9282f757e4758a5e1177
TiSeb <boissiere.seb@gmail.com>
2020-04-06 11:18:10 +1000

    Fix lint

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Contact = ({ onClick }) => {
+const Contact = ({ onClick, profileName }) => {
   return (
     <div className="contact">
       <h3 className="text-ssw-red">
