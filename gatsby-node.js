00fe93e4ac8d98915ecfde31e9c59634ec43e28a
Christian Morford-Waite <ChristianMWaite@ssw.com.au>
2020-04-29 14:45:16 +1000

    Added redirection rewrites for alumni

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,7 @@ const makePluginData = require('./src/helpers/plugin-data');
 const createRewriteMapsFile = require('./src/helpers/createRewriteMap');
 
 let assetsManifest = {};
+const alumniPrefix = '/alumni';
 
 exports.onCreateWebpackConfig = ({ stage, getConfig, actions }) => {
   const config = getConfig();
@@ -119,7 +120,7 @@ exports.createPages = async function({ actions, graphql }) {
       ? edge.node.childMarkdownRemark.frontmatter.nickname
       : null;
     const slug = edge.node.name;
-    const prefix = '';
+    const prefix = isCurrent ? '' : alumniPrefix.substring(1) + '/';
     return {
       slug: slug,
       path: prefix + slug.toLowerCase(),
@@ -154,14 +155,34 @@ exports.onPostBuild = async ({ store, pathPrefix }) => {
   const rewrites = Array.from(pages.values())
     .filter(
       page =>
-        page.context.nicknamePath &&
-        page.context.originalPath !== page.context.nicknamePath
+        (page.context.nicknamePath &&
+        page.context.originalPath !== page.context.nicknamePath)
     )
     .map(page => {
+      if(page.path.startsWith(alumniPrefix))
+      {
+        return {
+          fromPath: pathPrefix + '/' + page.context.originalPath.replace(alumniPrefix.substring(1) + '/',""), 
+          toPath: pathPrefix + page.path,
+        };
+      } else {
+        return {
+          fromPath: pathPrefix + '/' + page.context.originalPath,
+          toPath: pathPrefix + page.path,
+        };
+      }
+    });
+
+  const alumniRewrites = Array.from(pages.values())
+  .filter(
+    page =>
+      page.path.startsWith(alumniPrefix)
+  )
+  .map(page => {
       return {
-        fromPath: pathPrefix + '/' + page.context.originalPath,
+        fromPath: pathPrefix + '/' + page.path.replace(alumniPrefix + '/',""), 
         toPath: pathPrefix + page.path,
       };
-    });
-  await createRewriteMapsFile(pluginData, rewrites);
+  });
+  await createRewriteMapsFile(pluginData, rewrites.concat(alumniRewrites));
 };
