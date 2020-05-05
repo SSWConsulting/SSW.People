99f4df9c287e6d3e37571d1da13af962771fe0bf
SebastienBoissiere <boissiere.seb@gmail.com>
2020-05-05 14:03:39 +1000

    Fix Sitemap (#160)
    * Fix Sitemap
    
    * lint fix
    
    * Fix '//' issue

--- site-config.js
+++ site-config.js
@@ -7,7 +7,7 @@ module.exports = {
   siteDescription:
     `We work together to form an amazing collective brain - SSW is made up of a great team of staff that are passionate about technology and how it meets business needs!` +
     `We're enthusiastic and have a "Make it happen" culture.`,
-  siteUrl: `https://ssw.com.au/people`,
+  siteUrl: `https://ssw.com.au`,
   themeColor: `#cc4141`,
   backgroundColor: `#fff`,
   pathPrefix: null,
