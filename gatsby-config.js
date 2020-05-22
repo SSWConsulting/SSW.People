dc6f7698422022f3034bae513f0b5b25d4d058c9
TiSeb <boissiere.seb@gmail.com>
2020-05-22 11:42:03 +1000

    Back to cn

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,7 @@ require('dotenv').config({
 
 module.exports = {
   pathPrefix: `/people${
-    process.env.CHINA_BUILD && process.env.CHINA_BUILD === 'TRUE'
-      ? '-china'
-      : ''
+    process.env.CHINA_BUILD && process.env.CHINA_BUILD === 'TRUE' ? '-cn' : ''
   }`,
   siteMetadata: {
     ...siteConfig,
