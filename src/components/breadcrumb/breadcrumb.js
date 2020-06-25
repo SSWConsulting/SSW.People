9e00871e108c3a068ea361bad287d3024300b93e
Sebastien Boissiere [SSW] <boissiere.seb@gmail.com>
2020-06-25 17:10:52 +1000

    Fix print css (#195)
    * Fix footer for Ipad
    Fix print css
    
    * Change layout on Ipad
    
    * Fix image merge
    
    * Fix duplicate ids
    
    * Fix print layout
    
    * Hide Videos, audio and Talks buttons
    
    * Modify Breadcrumb css
    
    * Update menu
    
    * Fix layout on Ipad
    
    * Fix links in header
    
    * Fix links

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Breadcrumb } from 'gatsby-plugin-breadcrumb';
-import { parentSiteUrl, companyName } from '../../../site-config';
+import { parentSiteUrl } from '../../../site-config';
+import Icon from '../../images/branding/icon.png';
 
 const Breadcrumbs = ({ crumbs, crumbLabel }) => {
   return (
     <div className="breadcrumb-container print-hidden">
       <div className="mx-2 md:mx-6 mb-3 breadcrumb">
-        <a href={parentSiteUrl}>{companyName}</a>
+        <a href={parentSiteUrl}>
+          <img alt={'SSW Consulting'} src={Icon} className="w-4" />
+        </a>
         <span> &gt; </span>
         <Breadcrumb
           crumbs={crumbs}
