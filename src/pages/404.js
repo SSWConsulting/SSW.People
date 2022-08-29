c75404509493390ea65650a2a4235749c609ea86
Geordie Robinson [SSW] <97079655+Geordie88@users.noreply.github.com>
2022-08-29 16:14:05 +1000

    ✨ 404 page #419 - edited 404 text (#431)
    * ✨ 404 page #419 - edited 404 text
    
    * 🚨  Remove unused import
    
    Co-authored-by: Brady Stroud <bradystroud@ssw.com.au>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { navigate } from 'gatsby';
 
 import '../404.css';
 
@@ -12,27 +11,26 @@ const NotFound = () => {
             <h1 className="unselectable">404</h1>
             <div className="not-found-message">
               <h2 className="unselectable mt-2">
-                <span>PAGE NOT FOUND</span>
+                <span>PAGE NOT FOUND!</span>
                 <h2 className="unselectable -mt-4">
-                  Sorry, we could not find the
+                  Sorry, we couldn&apos;t find the
                 </h2>
+
                 <h2 className="unselectable -mt-6">
-                  page you were looking for
+                  page you were looking for...
                 </h2>
               </h2>
             </div>
             <div className="not-found-greybox greybox">
-              Visit <a href="https://www.ssw.com.au/ssw/">SSW homepage</a> for
-              details on our Services, Staff and more or
-              <span> </span>
-              <button onClick={() => navigate(-1)}>go back</button>
-              <span> </span>to the previous page.
+              Visit <a href="https://www.ssw.com.au/ssw/">SSW homepage</a> to
+              find out how we can help you.
             </div>
             <div className="not-found-greybox greybox">
-              This page is as per{' '}
+              Learn more about{' '}
               <a href="https://ssw.com.au/rules/404-useful-error-page">
-                Do you replace the 404 error with a useful error page?
+                having a useful 404 error page
               </a>
+              .
             </div>
           </div>
         </div>
