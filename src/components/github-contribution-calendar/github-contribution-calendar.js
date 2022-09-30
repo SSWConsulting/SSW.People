aa5269d68dc35b3b9debf4fe10ade5d1d94d6359
Brady Stroud <bradystroud@ssw.com.au>
2022-09-30 10:58:29 +1000

    Removed some packages

--- src/components/github-contribution-calendar/github-contribution-calendar.js
+++ src/components/github-contribution-calendar/github-contribution-calendar.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import GitHubCalendar from 'react-github-calendar';
-import PropTypes from 'prop-types';
-import ReactTooltip from 'react-tooltip';
+// import React from 'react';
+// import GitHubCalendar from 'react-github-calendar';
+// import PropTypes from 'prop-types';
+// import ReactTooltip from 'react-tooltip';
 
-const GitHubContributionCalendar = ({ githubUrl }) => {
-  let userName = githubUrl.split('/');
-  return (
-    <>
-      <hr />
-      <h2>GitHub Contributions</h2>
-      <div className="mx-3">
-        <GitHubCalendar
-          username={userName[userName.length - 1]}
-          blockSize={9}
-          blockMargin={3}
-          fontSize={10}
-        >
-          <ReactTooltip delayShow={50} html />
-        </GitHubCalendar>
-      </div>
-    </>
-  );
-};
+// const GitHubContributionCalendar = ({ githubUrl }) => {
+//   let userName = githubUrl.split('/');
+//   return (
+//     <>
+//       <hr />
+//       <h2>GitHub Contributions</h2>
+//       <div className="mx-3">
+//         <GitHubCalendar
+//           username={userName[userName.length - 1]}
+//           blockSize={9}
+//           blockMargin={3}
+//           fontSize={10}
+//         >
+//           <ReactTooltip delayShow={50} html />
+//         </GitHubCalendar>
+//       </div>
+//     </>
+//   );
+// };
 
-GitHubContributionCalendar.propTypes = {
-  githubUrl: PropTypes.string.isRequired,
-};
+// GitHubContributionCalendar.propTypes = {
+//   githubUrl: PropTypes.string.isRequired,
+// };
 
-export default GitHubContributionCalendar;
+// export default GitHubContributionCalendar;
