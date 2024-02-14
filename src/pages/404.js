255c6b2c7de731b7790ad852d7a43ce4d64eda1e
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2024-02-14 11:02:31 +1000

    updated 404 styling to fix resize issues (#603)

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,41 +1,31 @@
 import React from 'react';
-
 import '../404.css';
 
 const NotFound = () => {
   return (
-    <>
-      <div className="main-container">
-        <div className="not-found-page">
-          <div className="not-found-grid">
-            <h1 className="unselectable">404</h1>
-            <div className="not-found-message">
-              <h2 className="unselectable mt-2">
-                <span>PAGE NOT FOUND!</span>
-                <h2 className="unselectable mt-4">
-                  Sorry, we couldn&apos;t find the
-                </h2>
-
-                <h2 className="unselectable mt-6">
-                  page you were looking for...
-                </h2>
-              </h2>
-            </div>
-            <div className="not-found-greybox greybox">
-              Visit <a href="https://www.ssw.com.au/ssw/">SSW homepage</a> to
-              find out how we can help you.
-            </div>
-            <div className="not-found-greybox greybox">
-              Learn more about{' '}
-              <a href="https://ssw.com.au/rules/404-useful-error-page">
-                having a useful 404 error page
-              </a>
-              .
-            </div>
+    <div className="main-container">
+      <div className="not-found-page">
+        <div className="not-found-grid">
+          <h1 className="unselectable">404</h1>
+          <div className="not-found-message">
+            <h2 className="unselectable">
+              <span>PAGE NOT FOUND</span> <br />
+              Sorry, we couldn&apos;t find the page you were looking for.
+            </h2>
+          </div>
+          <div className="not-found-greybox greybox">
+            Visit <a href="https://ssw.com.au">SSW homepage</a> to find out how
+            we can help you.
+          </div>
+          <div className="not-found-greybox greybox">
+            Learn more about{' '}
+            <a href="https://ssw.com.au/rules/404-useful-error-page">
+              having a useful 404 error page.
+            </a>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 export default NotFound;
