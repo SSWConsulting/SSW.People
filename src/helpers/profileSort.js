3f39f31019d18aebc695460e06c6215d74833a4d
Brook Jeynes [SSW] <jeynesbrook@gmail.com>
2024-02-20 10:33:06 +1000

    Upgrade packages (#606)
    * Updated packages
    - assert 2.0.0->2.1.0
    - axios 1.1.3->1.6.7
    - classnames 2.2.6->2.5.1
    - applicationinsights 2.3.6->2.9.3
    - autoprefixer 10.4.13->10.4.17
    - css-loader 6.7.2->6.10.0
    - directory-named-webpack-plugin 4.0.0->4.1.0
    - dotenv 16.0.3->16.4.4
    
    * Updated packages
    - @raae/gatsby-remark-oembed 0.3.2->0.3.3
    - @types/youtube 0.0.47->0.0.50
    - gatsby-plugin-breadcrumb 12.3.1->12.3.2
    - postcss 8.4.31->8.4.35
    - postcss-nested 6.0.0->6.0.1
    - react-pose 4.0.5->4.0.10
    - styled-components 5.3.6->5.3.11
    - url 0.11.0->0.11.3
    - @babel/eslint-parser 7.23.3->7.23.10
    - applicationinsights 2.9.3->2.9.4
    - chrome-launcher 0.15.1->0.15.2
    - prettier 2.8.0->2.8.8
    
    * Updated packages
    - react-github-calendar 4.0.0->4.1.2
    - react-google-recaptcha 2.1.0->3.1.0
    - react-tooltip 5.16.1->5.26.3
    - react-youtube 10.0.0->10.1.0
    Please enter the commit message for your changes. Lines starting
    
    * Updated packages
    - moment 2.29.4->2.30.1
    - node-fetch 2.6.7->2.7.0
    - postcss-import 15.0.0->15.1.0
    - prop-types 15.6.2->15.8.1
    - query-string 8.1.0->8.2.0
    - rollup 3.8.0->3.29.4
    - ssw.megamenu 4.1.0->4.2.2
    - typescript 5.1.6->5.3.3
    
    * Updated packages
    - @babel/core 7.20.7->7.23.9
    - @fortawesome/free-brands-svg-icons 6.2.0->6.5.1
    - @fortawesome/free-regular-svg-icons 6.2.0->6.5.1
    - @microsoft/applicationinsights-web 3.0.2->3.1.0
    - @reach/router 1.2.1->1.3.4
    - gatsby 5.12.11->5.13.3
    - gatsby-plugin-google-tagmanager 5.3.1->5.13.1
    - gatsby-plugin-image 3.3.2->3.13.1
    - gatsby-plugin-postcss 6.1.0->6.13.1
    - gatsby-plugin-react-helmet 6.3.0->6.13.1
    - gatsby-plugin-sitemap 6.3.1->6.13.1
    - gatsby-remark-copy-linked-files 6.3.0->6.13.1
    - gatsby-transformer-json 5.3.0->5.13.1
    - lottie-web 5.7.3->5.12.2
    - @fortawesome/fontawesome-svg-core 6.2.0->6.5.1
    - @fortawesome/free-solid-svg-icons 6.2.0->6.5.1
    - eslint 8.54.0->8.56.0
    - gatsby-cli 5.3.1->5.13.2
    
    * Updated eslint
    - Added core-js and @babel/preset-react
    - eslint-plugin-prettier 4.2.1->5.1.3
    - prettier 2.8.8->3.2.5
    - Updated .eslintrc to match rules
    
    * updated packages

--- src/helpers/profileSort.js
+++ src/helpers/profileSort.js
@@ -12,16 +12,16 @@ export const JobSort = (a, b) => {
   return a.profile.jobTitle?.toLowerCase() < b.profile.jobTitle?.toLowerCase()
     ? -1
     : a.profile.jobTitle?.toLowerCase() > b.profile.jobTitle?.toLowerCase()
-    ? 1
-    : 0;
+      ? 1
+      : 0;
 };
 
 export const AlphabeticalSort = (a, b) => {
   return a.sanitisedName.toLowerCase() < b.sanitisedName.toLowerCase()
     ? -1
     : a.sanitisedName.toLowerCase() > b.sanitisedName.toLowerCase()
-    ? 1
-    : 0;
+      ? 1
+      : 0;
 };
 
 export const SampleProfileSort = (a, b) => {
