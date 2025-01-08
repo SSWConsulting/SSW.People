af8c2432de0d3142acdae0d29ac1e6d1cd1493e8
Christian Morford-Waite [SSW] <ChristianMWaite@ssw.com.au>
2025-01-09 10:37:52 +1100

    722 - Fix YouTube playlist widget (#723)
    * Moved youtube playlist generation to be on build
    
    * refactored and cleaned up youtube-playlist
    
    * removed the node-fetch require

--- src/templates/person.js
+++ src/templates/person.js
@@ -181,6 +181,7 @@ const Person = ({ pageContext }) => {
                 <hr />
                 <YoutubePlaylist
                   youtubePlayListId={crmData.youTubePlayListId}
+                  playlistItems={pageContext.data.playlistItems}
                 />
               </>
             )}
