396723b7d0c0d409c8bddd49eb3b865881e9b6cf
Tiago Araújo [SSW] <tiagov8@gmail.com>
2026-02-06 17:00:39 -0800

    Adjust breadcrumb container margins

--- src/components/breadcrumb/breadcrumb.js
+++ src/components/breadcrumb/breadcrumb.js
@@ -7,7 +7,7 @@ import Icon from '../../images/branding/icon.png';
 const Breadcrumbs = ({ crumbs, crumbLabel }) => {
   return (
     <div className="breadcrumb-container print-hidden">
-      <div className="mx-2 md:mx-6 mb-3 breadcrumb">
+      <div className="mx-6 md:mx-8 breadcrumb">
         <a href={parentSiteUrl}>
           <img alt={'SSW Consulting'} src={Icon} className="w-4" />
         </a>
