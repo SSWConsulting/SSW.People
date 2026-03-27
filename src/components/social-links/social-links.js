670c61d17b61523e4b8340fa58018ed0f305c1f1
Tiago Araújo [SSW] <tiagov8@gmail.com>
2026-03-27 14:27:59 -0700

    Update className for favor-content div

--- src/components/social-links/social-links.js
+++ src/components/social-links/social-links.js
@@ -60,7 +60,7 @@ const SocialLinks = ({ crmData, alumni }) => {
   return (
     <>
       {displayCrm && (
-        <div className="favor-content w-full print-hidden">
+        <div className="favor-content md:my-4 p-2 rounded w-full print-hidden">
           <ul className="favor-list mx-4 my-2">
             {crmData.emailAddress && (
               <>
