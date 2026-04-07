81b746654da12d7bfcdc5d36de5ee92ea3e56dfc
Tiago Araújo [SSW] <tiagov8@gmail.com>
2026-04-07 16:59:51 -0700

    Update layout.js

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -11,7 +11,7 @@ import { Toaster } from 'react-hot-toast';
 const Layout = ({ children, pageTitle, crumbs, crumbLabel }) => {
   return (
     <div>
-      <div className="flex flex-col min-h-screen w-fit main-container mt-2">
+      <div className="flex flex-col min-h-screen w-fit main-container mt-4">
         <Toaster position="top-center" reverseOrder={false} />
         <Head pageTitle={pageTitle} />
         <Header />
