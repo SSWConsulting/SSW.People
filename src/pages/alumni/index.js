e967eac5e9a1a078eb7e1c4b4102851777fbdb55
Tiago Araújo [SSW] <tiagov8@gmail.com>
2026-04-08 18:39:40 -0700

    Update index.js

--- src/pages/alumni/index.js
+++ src/pages/alumni/index.js
@@ -81,7 +81,7 @@ const Index = ({ data }) => {
           onLocationChange={setSelectedLocation}
         />
       </div>
-      <div className="mx-2 md:mx-6 flex flex-col lg:flex-row">
+      <div className="mx-4 flex flex-col lg:flex-row">
         <div className="lg:w-1/4">
           <div className="mx-auto flex flex-col sm:flex-row lg:flex-col lg:w-5/6">
             <div className="w-full sm:w-1/2 lg:w-full">
